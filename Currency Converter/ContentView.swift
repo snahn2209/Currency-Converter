3e3040c51dcc6cf3a29b4bd8d2b4c6185a7adbe8
snahn2209 <106750285+snahn2209@users.noreply.github.com>
2023-11-28 09:40:03 +0100

    Initial Commit

---
+++ Currency Converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Currency Converter
+//
+//  Created by Sina Han on 28.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Currency Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency Converter
+//
+//  Created by Sina Han on 28.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
